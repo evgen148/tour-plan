c552378c3af860f47af487d3c2b2eb1ccde900fc
evgen148 <Kazama1390@yandex.ru>
2020-12-11 18:25:48 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -29,3 +29,10 @@ var reviewsSlider = new Swiper(".reviews-slider", {
   },
   
 })
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function (){
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+});
