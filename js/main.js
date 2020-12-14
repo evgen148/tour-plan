65bfc32d52ec68e2bd66c9009466beab75312778
evgen148 <Kazama1390@yandex.ru>
2020-12-14 21:41:10 +0300

    a

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelslider = new Swiper(".hotel-slider", {
+$(document).ready(function(){
+ var hotelslider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
@@ -26,13 +27,33 @@ var reviewsSlider = new Swiper(".reviews-slider", {
   keyboard: {
     enabled: true,
     onlyInViewport: false,
-  },
-  
+  }, 
 })
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener('click', function (){
   document
   .querySelector(".navbar-bottom")
-  .classList.toggle("navbar-bottom--visible");
-});
+  .classList.toggle("navbar-bottom--visible") 
+}); 
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal(){
+  var targetModal = $(this).attr("data-href");
+  $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
+  $(targetModal).find(".modal__dialog").addClass('modal__dialog--visible');
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+  $(modal_overlay).modal({ backdrop: 'static', keyboard: false });
+}
+
+})
---
+++ send.php
@@ -0,0 +1,60 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$adress = $_POST['adress'];
+
+
+// Формирование самого письма
+$title = "Новое обращениу Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message
+<b>Адрес: <b> $adress
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'kazama9013@gmail.com'; // Логин на почте
+    $mail->Password   = 'Kazama1390!'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('kazama9013@gmail.com', 'Евгений Горковенко'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('Kazama1390@yandex.ru');  
+    
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
