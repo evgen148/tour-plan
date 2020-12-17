f64b06bbfe2168b230d74a89b263a3ad3bd0da82
evgen148 <Kazama1390@yandex.ru>
2020-12-17 19:15:33 +0300

    adds

--- js/main.js
+++ js/main.js
@@ -44,16 +44,22 @@ closeModalButton.on('click', closeModal);
 
 function openModal(){
   var targetModal = $(this).attr("data-href");
+  var body = $("body");
+
   $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
   $(targetModal).find(".modal__dialog").addClass('modal__dialog--visible');
+  body.addClass("scroll-hidden");
 }
 function closeModal(event) {
   event.preventDefault();
   var modalOverlay = $(".modal__overlay");
   var modalDialog = $(".modal__dialog");
+  var body = $("body");
+
   modalOverlay.removeClass("modal__overlay--visible");
   modalDialog.removeClass("modal__dialog--visible");
-  $(modal_overlay).modal({ backdrop: 'static', keyboard: false });
+  //$(modal_overlay).modal({ backdrop: 'static', keyboard: false });
+  body.removeClass("scroll-hidden");
 }
 
-})
+});
